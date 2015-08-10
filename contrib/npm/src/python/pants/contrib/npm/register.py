837940ccf63b49be9701cb10cdac832f22bfcd7c
Stu Hood <stuhood@gmail.com>
2015-08-10 12:46:47 -0700

    Slurp version into npm_module targets as well.

--- contrib/npm/src/python/pants/contrib/npm/register.py
+++ contrib/npm/src/python/pants/contrib/npm/register.py
@@ -5,20 +5,24 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-from pants.base.target import Target
 from pants.base.build_file_aliases import BuildFileAliases
+from pants.base.exceptions import TargetDefinitionException
+from pants.base.target import Target
 from pants.goal.task_registrar import TaskRegistrar as task
 
 
 class NpmModule(Target):
-  def __init__(self, main=None, resources=None, *args, **kwargs):
-    # TODO: payload
+  def __init__(self, version=None, main=None, resources=None, *args, **kwargs):
     super(NpmModule, self).__init__(*args, **kwargs)
+    if not version:
+      raise TargetDefinitionException(self, "The `version` argument is required, and should specify a semantic version.")
+
 
 class NpmRemoteModule(Target):
-  def __init__(self, rev=None, *args, **kwargs):
-    # TODO: payload
+  def __init__(self, version=None, *args, **kwargs):
     super(NpmRemoteModule, self).__init__(*args, **kwargs)
+    if not version:
+      raise TargetDefinitionException(self, "The `version` argument is required, and should specify a semantic version.")
 
 
 def build_file_aliases():
