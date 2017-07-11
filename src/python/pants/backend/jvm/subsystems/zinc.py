e86549c8f2254965692f9ae284fce69bbfa3d6ab
Stu Hood <stuhood@gmail.com>
2017-07-11 11:59:51 -0700

    Victory

--- src/python/pants/backend/jvm/subsystems/zinc.py
+++ src/python/pants/backend/jvm/subsystems/zinc.py
@@ -26,7 +26,7 @@ class Zinc(Subsystem, JvmToolMixin):
 
   ZINC_EXTRACTOR_TOOL_NAME = 'zinc-extractor'
 
-  _ZINC_VERSION = 'stuhood-zinc-1.0.0-X19-M1'
+  _ZINC_VERSION = 'stuhood-zinc-1.0.0-X19-M1-4'
 
   @classmethod
   def register_options(cls, register):
